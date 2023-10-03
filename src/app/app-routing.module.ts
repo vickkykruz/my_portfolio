cb7313caf2a70892c617206d38babae1ca5d76cf
VickkyProgrammer <73972088+vickkykruz@users.noreply.github.com>
2023-10-02 19:51:02 -0700

    Did some adjustment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { NotfoundComponent } from './notfound/notfound.component';
 import { ServiceComponent } from './service/service.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about-me', component: AboutMeComponent},
-  {path: 'service', component: ServiceComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: '**', component: NotfoundComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'service', component: ServiceComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent}
 ];
 
 @NgModule({
